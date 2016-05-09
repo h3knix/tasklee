235397f2febeee98a23d774e2da81211a060d81d
Mike <h3knix@gmail.com>
2016-05-08 22:12:53 -0600

    move pdo opts to conf

--- lib/app.php
+++ lib/app.php
@@ -3,15 +3,10 @@
 	class app {
 		public static $conf;
 		
-		public static $pdo_opts = array(
-			//my php version happend to be pre 5.3.6 and i want utf8
-		    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-		);
-		
 		public static $db;
 		public static function db_connect($conf=null) {
 			if ( ! $conf ) $conf = self::$conf;
-			self::$db = new PDO($conf->db_dsn, $conf->db_user, $conf->db_pass, self::$pdo_opts);
+			self::$db = new PDO($conf->db_dsn, $conf->db_user, $conf->db_pass, $conf->pdo_opts);
 			return self::$db;
 		}
 	}
--- lib/conf.php
+++ lib/conf.php
@@ -6,4 +6,9 @@
 		public $db_dsn = 'mysql:host=localhost;dbname=tasklee;charset=utf8';
 		public $db_user = 'tasklee';
 		public $db_pass = '';
+		
+		public $pdo_opts = array(
+			//my php version happend to be pre 5.3.6 and i want utf8
+		    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+		);
 	}
